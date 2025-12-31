aea1dfdcf576c64c0b42f1d51e6348dbb7727ad0
Daito-S <218743916+Daito-S@users.noreply.github.com.>
2026-01-01 01:41:19 +0900

    複数のaiを切り替えて試せるように改善

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from ai.simple_ai import Simple_ai
+from ai.markov_ai import Markov_ai
 from hand_enum import Hand
 
-ai_simple = Simple_ai() 
-hand_dict = {'グー':Hand.ROCK, 'チョキ':Hand.SCISSORS, 'パー':Hand.PAPER}
+def janken():
+  for i in range(10):
+    x = input("グー、チョキ、パー　を入力してください。")
+    assert x in hand_dict, "グー・チョキ・パーと違うね？"
 
-for i in range(10):
-  x = input("グー、チョキ、パー　を入力してください。")
+    ai_hand = [k for k, v in hand_dict.items() if v == AI.prediction()][0]
+    print(f'AI：{ai_hand}')
+    print()
+    AI.learn(hand_dict[x])
+  print(AI.record)
 
-  ai_hand = [k for k, v in hand_dict.items() if v == ai_simple.prediction()][0]
-  print(f'AI：{ai_hand}')
-  print()
-  ai_simple.learn(hand_dict[x])
+hand_dict = {'グー': Hand.ROCK, 'チョキ': Hand.SCISSORS, 'パー': Hand.PAPER}
+
+'''メイン処理'''
+# AIの種類を決定
+ai_num = input('シンプル：1, マルコフ：2を入力')
+if ai_num == "1":
+  AI = Simple_ai()
+elif ai_num == "2":
+  AI = Markov_ai()
+
+janken()
+
+if input('続けるなら入力せずEnter') == "":
+  janken()
