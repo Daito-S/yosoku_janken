dd3a1cb77d2c9e1bad5e90212d43628230eda05d
Daito-S <218743916+Daito-S@users.noreply.github.com.>
2026-01-01 23:14:11 +0900

    main.pyをpygameの初期設定に更新。

--- main.py
+++ main.py
@@ -1,29 +1,8 @@
-from ai.simple_ai import Simple_ai
-from ai.markov_ai import Markov_ai
-from hand_enum import Hand
+import pygame as pg
+import config as cf
 
-def janken():
-  for i in range(10):
-    x = input("グー、チョキ、パー　を入力してください。")
-    assert x in hand_dict, "グー・チョキ・パーと違うね？"
+# <-----初期設定----->
 
-    ai_hand = [k for k, v in hand_dict.items() if v == AI.prediction()][0]
-    print(f'AI：{ai_hand}')
-    print()
-    AI.learn(hand_dict[x])
-  print(AI.record)
-
-hand_dict = {'グー': Hand.ROCK, 'チョキ': Hand.SCISSORS, 'パー': Hand.PAPER}
-
-'''メイン処理'''
-# AIの種類を決定
-ai_num = input('シンプル：1, マルコフ：2を入力')
-if ai_num == "1":
-  AI = Simple_ai()
-elif ai_num == "2":
-  AI = Markov_ai()
-
-janken()
-
-if input('続けるなら入力せずEnter') == "":
-  janken()
+pg.init()
+screen = pg.display.set_mode((cf.WIDTH, cf.HEIGHT))
+pg.display.set_caption('予測じゃんけん')
